b71f8ccf67e978b43a617d071c0142e5bc3de070
Eve Kozina <eve.kozina@gmail.com>
2022-01-11 13:44:31 -0500

    commit tree 58094a4667c6597b03c50977372f381980a3302f

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entry_response
 
   # GET /birds
   def index
@@ -45,6 +46,10 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entry_response
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entry
   end
 
 end
